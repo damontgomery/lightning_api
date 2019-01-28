0e891d29adbd3407effd33ff277c847b675c5576
Adam Hoenich <adam.hoenich@acquia.com>
2019-01-28 13:30:28 -0500

    Always rebuild the router and clear the route provider cache when creating a bundle entity.

--- tests/src/FixtureContext.php
+++ tests/src/FixtureContext.php
@@ -18,15 +18,7 @@ final class FixtureContext extends FixtureBase {
       ->set('bundle_docs', TRUE)
       ->save();
 
-    // Lightning Core's FixtureContext installs modules, so we'll want to get
-    // the current container to be sure we don't run into weird errors.
-    /** @var \Symfony\Component\DependencyInjection\ContainerInterface $container */
-    $container = $this->container->get('kernel')->getContainer();
-    $this->setContainer($container);
-
-    // Clear important caches to expose the new content type as needed.
-    $container->get('router.builder')->rebuild();
-    $container->get('entity_type.bundle.info')->clearCachedBundles();
+    $this->container->get('entity_type.bundle.info')->clearCachedBundles();
   }
 
   /**
